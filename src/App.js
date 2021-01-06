a74151ede744dd70463b0bd738cb5e80399932e8
Alwin Arrasyid <alwin.ridd@gmail.com>
2021-01-06 22:24:45 +0700

    Using react-autosuggest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState } from "react";
+import AutoSuggest from "react-autosuggest";
+import "./App.css";
+
+const languages = [
+  {
+    name: "C",
+    year: 1972,
+  },
+  {
+    name: "Elm",
+    year: 2012,
+  },
+];
+
+const getSuggestions = (value) => {
+  const inputValue = value.trim().toLowerCase();
+  const inputLength = inputValue.length;
+
+  return inputLength === 0
+    ? []
+    : languages.filter(
+        (lang) =>
+          lang.name.toLocaleLowerCase().slice(0, inputLength) === inputValue
+      );
+};
+
+const getSuggestionValue = (suggestion) => suggestion.name;
+
+const renderSuggestion = (suggestion) => <div>{suggestion.name}</div>;
 
 function App() {
+  const [value, setValue] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  const onSuggestionFetchRequested = useCallback(({ value }) => {
+    setSuggestions(getSuggestions(value));
+  }, []);
+
+  const onSuggestionClearRequested = useCallback(() => {
+    setSuggestions([]);
+  }, []);
+
+  const onChange = useCallback((event, { newValue }) => {
+    setValue(newValue);
+  }, []);
+
+  const inputProps = {
+    placeholder: "Type a programming language",
+    value,
+    onChange,
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AutoSuggest
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={onSuggestionFetchRequested}
+        onSuggestionsClearRequested={onSuggestionClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputProps}
+      />
     </div>
   );
 }
